2c4a812b6a73332adc3ed878f9f521ee40510711
amankishorelearning <aman.kishore.learning@gmail.com>
2026-03-22 12:16:03 +0100

    Lab2 Commit

---
+++ JAVA_1/Lab2/src/com/jlc/lab2/Lab2.java
@@ -0,0 +1,16 @@
+/**
+ * Write a Java program to display the default values of the data types of Java.
+ */
+package com.jlc.lab2;
+
+/**
+ * @author Aman Kishore
+ *
+ */
+public class Lab2 {
+	public static void main(String[] args) {
+		showValues sv = new showValues();
+		sv.show();
+	}
+
+}
