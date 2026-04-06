6ce4ab6900bba516d20b6796751686bc0e4b3f6b
amankishorelearning <aman.kishore.learning@gmail.com>
2026-04-06 14:09:34 +0200

    Lab7 - Lab9

---
+++ JAVA_1/Lab7/src/com/jlc/lab7/Lab7.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.jlc.lab7;
+
+/**
+ * @author Aman Kishore
+ * Objective: declaration of instance variable and intialization in the same line
+ * Learning: Declaration and initialization of the variable can happen in the same line. 
+ */
+public class Lab7 {
+	public static void main(String[] args) {
+		int a = 99;
+		boolean b = true;
+		double d = 999.99;
+		String str = "Aman";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(d);
+		System.out.println(str);
+		
+	}
+
+}
---
+++ JAVA_1/Lab8/src/com/jlc/lab8/Lab8.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.jlc.lab8;
+
+/**
+ * @author Aman Kishore
+ * Objective: declaring multiple variables of different data types in one line
+ * Learning: different data type in one line is not accepted.
+ */
+public class Lab8 {
+	public static void main(String[] args) {
+//		int a, byte b;
+//		System.out.println(a);
+//		System.out.println(b);
+	}
+
+}
