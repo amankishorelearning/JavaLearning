dca153253a01c3a8a5b5b35c8608055999faad8f
amankishorelearning <aman.kishore.learning@gmail.com>
2026-04-23 03:30:27 +0200

    Lab22 - Lab24

---
+++ JAVA_1/Lab22/src/com/jlc/lab22/Lab22.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.jlc.lab22;
+
+/**
+ * @author Aman Kishore
+ * Objective: Learning Character Literals
+ * Learning: 
+ */
+public class Lab22 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		char ch1 = 's';
+		char ch2 = 'j';
+		char ch3 = 'S';
+		char ch4 = '*';
+		
+		System.out.println(ch1);
+		System.out.println(ch2);
+		System.out.println(ch3);
+		System.out.println(ch4);
+		
+
+	}
+
+}
---
+++ JAVA_1/Lab24/src/com/jlc/lab24/Lab24.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.jlc.lab24;
+
+/**
+ * @author Aman Kishore
+ * Objective: Learning char literals
+ * Learning: working with space, tabs and escape characters in char is valid.
+ */
+public class Lab24 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		char ch1 = ' ';
+		char ch2 = '	';
+		char ch3 = '/';
+		
+		System.out.println(ch3);
+				
+		
+	}
+
+}
